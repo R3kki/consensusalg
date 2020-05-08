b5f167b8991f53cb6c6904cefb17e5a0bcde3de8
R3kki <richie.parkie@gmail.com>
2020-05-08 15:42:45 -0400

    network connectivity logic

--- src/CompliantNode.java
+++ src/CompliantNode.java
@@ -3,14 +3,14 @@ import java.util.*;
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
-    private final double p_graph;
-    private final double p_malicious;
-    private final double p_txDistribution;
+    private final double p_graph;           // % of connecting edges
+    private final double p_malicious;       // % of malicious nodes
+    private final double p_txDistribution;  // % of nodes having an initial set of transactions
     private final int numRounds;
+
+    private int numFollowees;
     private int currRound;
-    private Set<Integer> followees; // ??
-    private Set<Transaction> proposedTransactions;
-    private Map<Transaction, Integer> consensusCount;
+    private Map<Transaction, Set<Integer>> candidateTransactions;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         this.p_graph = p_graph;
@@ -18,54 +18,43 @@ public class CompliantNode implements Node {
         this.p_txDistribution = p_txDistribution;
         this.numRounds = numRounds;
         currRound = 0;
-
-        this.followees = new HashSet<>();
-        this.proposedTransactions = new HashSet<>();
-        this.consensusCount = new HashMap<>();
+        candidateTransactions = new HashMap<>();
     }
 
-    // initialize: nodes that this node follows
+    // initialize: nodes that this node follows (following nodes)
     public void setFollowees(boolean[] followees) {
         for (int i = 0; i < followees.length; i++)
-            if (followees[i]) this.followees.add(i);
+            if (followees[i]) numFollowees++;
     }
 
-    // initialize: the transactions randomly sent from the network
+    // initialize: the transactions randomly sent from the network according to prob dist p_txDistribution
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        for (Transaction tx : pendingTransactions) {
-            if (!this.proposedTransactions.contains(tx)) this.proposedTransactions.add(tx);
-            consensusCount.put(tx, consensusCount.getOrDefault(tx, 0) + 1);
-        }
+        for (Transaction tx : pendingTransactions)
+            candidateTransactions.put(tx, new HashSet<>());
     }
 
-    // send proposals to followers.
+    // send proposals to followers. And filter out for bad ones
     public Set<Transaction> sendToFollowers() {
         if (++currRound == numRounds) {
             // new behaviour: return transactions reached upon consensus
-            Queue<Transaction> consensus = new PriorityQueue<>(
-                    (a, b) -> consensusCount.get(b) - consensusCount.get(a)
-            );
-
-            // sort by transaction count
-            for (Transaction tx : consensusCount.keySet())
-                consensus.add(tx);
-
-
-            int numNodes = consensusCount.size();
-            int numTx = (int) (numNodes * (1 - p_malicious) );
-            proposedTransactions = new HashSet<>();
-            for (int i = 0; i < numTx; i++)
-                proposedTransactions.add(consensus.poll());
+            int numCompliantNodes = (int) (numFollowees * (1 - p_malicious));
+            for (Transaction tx : candidateTransactions.keySet()){
+                if (candidateTransactions.get(tx).size() < numCompliantNodes)
+                    candidateTransactions.remove(tx);
+            }
         }
-        return proposedTransactions;
+        return candidateTransactions.keySet();
     }
 
-    // Receive candidates from other nodes
+    // Receive candidates by listening to nodes that this node follows (following/followee)
+    // candidate sender is the followee
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        for (Candidate c : candidates) {
+        for (Candidate c : candidates){
+            Integer followee = c.sender;
             Transaction tx = c.tx;
-//            int sender = c.sender;
-            consensusCount.put(tx, consensusCount.getOrDefault(tx, 0) + 1);
+            if (!candidateTransactions.containsKey(tx)) candidateTransactions.put(tx, new HashSet<>());
+            candidateTransactions.get(tx).add(followee);
         }
+
     }
 }
